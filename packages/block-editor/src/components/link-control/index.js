2d2843643b6daa85bfe2394a1b79169c338df608
Dave Smith <getdavemail@gmail.com>
2020-02-05 16:14:20 +0000

    Reinstate keyboard handling of suggestion selection bug fix.
    Failing to pass the current input value as `url` of the suggestion causes the keyboard handling to break when hitting `ENTER`. Not entirely sure why at this stage.

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -231,8 +231,8 @@ function LinkControl( {
 			? results
 			: results.concat( {
 					id: cleanForSlug( val ),
-					title: val, // // must match the existing `<input>`s text value
-					url: '', // placeholder - not used when creating
+					title: val, // must match the existing `<input>`s text value
+					url: val, // must match the existing `<input>`s text value
 					type: CREATE_TYPE,
 			  } );
 	};
